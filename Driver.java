1089bf34b5185c8f3be2deeb56a2925e0e86728f
trstcod <35841518+trstcod@users.noreply.github.com>
2018-06-21 21:36:33 +0530

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -15,16 +15,12 @@ public class Driver {
 
         ReadInput ri = new ReadInput();
         ri.readBasic();
-      //  System.out.println("### READ FILE ###");
         SortRequest sr = new SortRequest();
         sr.sort(ri.getReq());
-      //  System.out.println("### BEFORE DIJKSTRAS ###");
-        Dijkstras dj = new Dijkstras();
-       // int[][] loc= new int[ri.getN()];
+        Dijkstras dj = new Dijkstras();  
         for(int i=0;i<ri.getN();i++) {
             Dijkstras.dij(ri.getLocation(),i,ri.getN());
         }
-       // System.out.println("### BEFORE SCHEDULING ###");
         Scheduling sd = new Scheduling();
         sd.schedule(ri.getCar(), ri.getReq(), ri.getLocation());
         OutputRevenue o = new OutputRevenue();
